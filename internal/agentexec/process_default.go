b7f3997adaebfe0d516a899ec8dc0a6c3a96b630
backinfile <1757518497@qq.com>
2026-08-25 14:13:01 +0800

    chore: clear project for full rewrite

--- internal/agentexec/process_default.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package agentexec
-
-import "os/exec"
-
-func runAgentCommand(cmd *exec.Cmd) error {
-	return cmd.Run()
-}
--- web/embed.go
+++
@@ -1,9 +0,0 @@
-package web
-
-import "embed"
-
-// Files contains the production UI. Keeping the assets next to this package lets
-// the Go compiler produce a self-contained ProjectBoard binary.
-//
-//go:embed index.html assets/*
-var Files embed.FS
